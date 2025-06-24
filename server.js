195b1f21b80abf7aa955d3b687ec72bf7ae2b362
rohanb684 <rohanb684@gmail.com>
2025-06-24 15:09:01 +0530

    fixed main domain

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:5175",
       "https://abaan-new.vercel.app",
       "https://snazzy-macaron-98dc05.netlify.app",
-      "https://abaandelights.com",
+      "https://www.abaandelights.com",
     ],
     credentials: true,
   })
